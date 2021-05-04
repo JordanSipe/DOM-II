c2c0853e1ad70067b7bb24df09294e7ef49dc907
JordanSipe <jsipe2017@gmail.com>
2021-05-04 16:36:37 -0400

    Finished Assignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+
+//Selectors
+const mainNav = document.querySelector(".main-navigation");
+const navContainer = mainNav.querySelector(".nav-container");
+const logoHead = navContainer.querySelector(".logo-heading");
+const nav = navContainer.querySelector(".nav");
+const anchors = nav.querySelectorAll(".nav-link");
+
+const body = document.querySelector(".home");
+const intro = document.querySelector(".intro");
+const introImg = intro.querySelector("img");
+const introHeader = intro.querySelector("h2");
+const introPara = intro.querySelector("p");
+
+
+
+// Event Listeners
+introHeader.addEventListener("mouseover", function(event) {
+    event.target.style.color = "white";
+});
+introHeader.addEventListener("mouseleave", function(event) {
+    event.target.style.color = "";
+});
+
+introPara.addEventListener("mouseover", function(event) {
+    event.target.style.color = "white";
+});
+introPara.addEventListener("mouseleave", function(event) {
+    event.target.style.color = "";
+});
